b2b450934d060ea46eba93c339014e60516c2bbc
DaneeXIII <szarkadani0302@gmail.com>
2021-04-27 20:30:32 +0200

    elso

--- app/src/main/java/com/example/kozosprojekt3/MainActivity.java
+++ app/src/main/java/com/example/kozosprojekt3/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kozosprojekt3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,8 +19,12 @@ public class MainActivity extends AppCompatActivity {
         regisztracioGomb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                masodikMegnyitas();
             }
         });
+    }
+    public void masodikMegnyitas(){
+        Intent intent = new Intent(this, MasodikActivity.class);
+        startActivity(intent);
     }
 }
